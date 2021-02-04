560f989d570b87df1119eb273d9a2d25636af0d9
= <=>
2021-02-04 15:29:13 +0700

    v for gh-pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store'
-import { homepage } from '../../../package.json';
 
 ReactDOM.render(
-  <BrowserRouter basename={ homepage }>
+  <BrowserRouter basename={ process.env.PUBLIC_URL }>
     <Provider store={ store }>
       <App />
     </Provider>
